54b47a992eeba4b42b02ffcc222e81ed6b2b19c9
ledai <ledai@paypal.com>
2026-04-14 10:49:53 +0800

    init

---
+++ src/main/java/com/example/financial/home/work/FinancialHomeWorkApplication.java
@@ -0,0 +1,12 @@
+package com.example.financial.home.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialHomeWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialHomeWorkApplication.class, args);
+    }
+}
